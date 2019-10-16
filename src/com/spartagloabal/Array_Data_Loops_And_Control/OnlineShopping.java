9c05240d2f87ddc5b51edd2f7b7485b601651a55
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-16 16:28:05 +0100

    Need Re-Coding Online shopping

--- src/com/spartagloabal/Array_Data_Loops_And_Control/OnlineShopping.java
+++ src/com/spartagloabal/Array_Data_Loops_And_Control/OnlineShopping.java
@@ -14,9 +14,9 @@ public class OnlineShopping {
             System.out.println(removeItem[i]);
             for(int j = 0; j < shoppingBasicArr.length; j++){
                 System.out.println(shoppingBasicArr[j]);
-                if (shoppingBasicArr[i] != removeItem[j]){
-                    updateBasicArr[iCount] = shoppingBasicArr[i];
-                    iCount++;
+//                if (shoppingBasicArr[i] != removeItem[j]){
+//                    updateBasicArr[iCount] = shoppingBasicArr[i];
+//                    iCount++;
                 }
 
             }
