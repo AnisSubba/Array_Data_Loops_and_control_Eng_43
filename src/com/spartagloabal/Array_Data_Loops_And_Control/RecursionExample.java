0757abc4ca88930232d2df8c4c3d2a3d24c138ff
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-16 14:45:39 +0100

    Recursion technique java workplace

--- src/com/spartagloabal/Array_Data_Loops_And_Control/RecursionExample.java
+++ src/com/spartagloabal/Array_Data_Loops_And_Control/RecursionExample.java
@@ -1,4 +1,11 @@
 package com.spartagloabal.Array_Data_Loops_And_Control;
 
 public class RecursionExample {
+
+    public int fibonacciEx(int i){
+        if (i <= 1) {
+            return i;
+        }
+        return fibonacciEx(i-1) + fibonacciEx(i-2);
+    }
 }
