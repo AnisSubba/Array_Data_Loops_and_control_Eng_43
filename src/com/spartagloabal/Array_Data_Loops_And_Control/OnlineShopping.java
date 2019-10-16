2d8b2caa39367bc04e4184162c01239ff0a7f748
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-16 14:43:57 +0100

    Trying Recursion java techniques

--- src/com/spartagloabal/Array_Data_Loops_And_Control/OnlineShopping.java
+++ src/com/spartagloabal/Array_Data_Loops_And_Control/OnlineShopping.java
@@ -1,4 +1,37 @@
 package com.spartagloabal.Array_Data_Loops_And_Control;
 
 public class OnlineShopping {
+    private String[] shoppingBasicArr = {"Milk", "Egg", "rice", "Pork", "Ramen"};
+    private String[] shoppingNiceToHaveArr = {"Chesse", "Shrimp", "Steak", "Truffle", "Lobster"};
+
+
+
+    public String[] newBasicArr(String[] removeItem){
+        int iCount = 0;
+        String[] updateBasicArr = new String[shoppingBasicArr.length - removeItem.length];
+
+        for(int i = 0; i < removeItem.length; i++){
+            System.out.println(removeItem[i]);
+            for(int j = 0; j < shoppingBasicArr.length; j++){
+                System.out.println(shoppingBasicArr[j]);
+                /*if (shoppingBasicArr[i] != removeItem[j]){
+                    updateBasicArr[iCount] = shoppingBasicArr[i];
+                    iCount++;
+                }*/
+
+            }
+        }
+        return updateBasicArr;
+    }
+//    public void printBasics()
+//    {
+//        for (String shoppingItem: shoppingBasicArr){
+//            System.out.println(shoppingItem);
+//        }
+//    }
+//    public void printNiceToHave(){
+//        for (String shoppingNiceToHave : shoppingNiceToHaveArr){
+//            System.out.println(shoppingNiceToHave);
+//        }
+//    }
 }
