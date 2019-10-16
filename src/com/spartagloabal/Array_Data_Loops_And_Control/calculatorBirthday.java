24ecacb778e1f7eeb3a6dbb1a50a4aafa5b28568
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-16 15:44:17 +0100

    Magic Mike birthday math trick solution in Java

--- src/com/spartagloabal/Array_Data_Loops_And_Control/calculatorBirthday.java
+++ src/com/spartagloabal/Array_Data_Loops_And_Control/calculatorBirthday.java
@@ -1,4 +1,27 @@
 package com.spartagloabal.Array_Data_Loops_And_Control;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class calculatorBirthday {
+    double month;
+    double day;
+
+    public void calBirthday(){
+        Scanner birthdayDay = new Scanner(System.in);
+        System.out.println("What is your birthday day? ");
+        day = birthdayDay.nextDouble();
+
+
+        Scanner birthdayMonth = new Scanner(System.in);
+        System.out.println("WHat is your birthday month?");
+        month = birthdayMonth.nextDouble();
+    }
+    public void birthDate(){
+        calBirthday();
+
+        double a = (((((((((((7 * month)-1)*13)+day)+3)*11)-month)-day)/10)+11)/100);
+        System.out.println(a);
+    }
 }
